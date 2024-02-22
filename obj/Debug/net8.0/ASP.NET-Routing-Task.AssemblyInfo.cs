a13299a6f6208234abdc65e2590b9f385bb0e063
OmarHamdyx <zoltarscorpio@gmail.com>
2024-02-22 23:22:26 +0200

    Update

--- obj/Debug/net8.0/ASP.NET-Routing-Task.AssemblyInfo.cs
+++ obj/Debug/net8.0/ASP.NET-Routing-Task.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET-Routing-Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e569af7dc0a3358568c6356fef303dcf8e0cea4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET-Routing-Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET-Routing-Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
